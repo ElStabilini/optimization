42f47af40382d15ea075027fdca4a9518cf3850b
ElStabilini <el.stabilini@gmail.com>
2024-10-08 16:12:30 +0200

    developing optimization routine

--- rb_optimization.py
+++ rb_optimization.py
@@ -3,30 +3,23 @@ parametri dell'impulso di DRAG'''
 
 from dataclasses import dataclass, field
 from typing import Optional
-import json
-import re
+from pathlib import Path
 
 import numpy as np
 import numpy.typing as npt
-from qibolab import AcquisitionType, AveragingMode, ExecutionParameters
 from qibolab.qubits import QubitId
-#from scipy.optimize import curve_fit
 from scipy.optimize import minimize
 
-from qibocal.auto.execute import Executor
 from qibocal import update
-from qibocal.config import log
-from qibocal.protocols.drag import _acquisition as drag_acquisition, _fit as drag_fit
-from qibocal.protocols.randomized_benchmarking.standard_rb import _acquisition as rb_acquisition, _fit as rb_fit
 from qibocal.auto.execute import Executor
 from qibocal.cli.report import report
 
 def test_rb_optimization(
-        executor, 
+        #executor, 
         platform,
         target,
         method,
-        init_guess #forse potrebbe essere utile fissarla all'interno su un valore teorico
+        init_guess
     ):
     
     #inizializzo l'esecutore
@@ -43,8 +36,11 @@ def test_rb_optimization(
         #? c'è un valore "migliore"? affidabile ma non troppo grande per risparmiare tempo?
 
         '''INSERIRE DRAG - non penso di poter eseguire l'intera routine in questo caso'''
-        drag_output = e.drag_tuning(beta_start=-4, beta_end=4, beta_step=0.5) #questa riga è da modificare
-        #eseguo darg con certi paramteri e poi trovo la ondevice_rb
+        drag_output = e.drag_tuning(
+            beta_start=-4, 
+            beta_end=4, 
+            beta_step=0.5
+        )
 
         #check parameters value (sono definiti interanmente perchè vario la rb e DRAG rimane fisso)
         rb_output = e.rb_ondevice(
@@ -55,24 +51,31 @@ def test_rb_optimization(
             save_sequences = True,
             apply_inverse = True
         ) 
-        #esegue tutta la routine, a me in realtà non serve
 
-    infidelity = re.search(r"Gate infidelity.*?(\d+\.\d+)", fitting_report)
-    
-    
-    res = minimize(infidelity, init_guess, method = method, options={'xatol': 1e-8, 'disp': True})
-    #? la funzione da minimizzare è l'RB?
-    #? l'initial guess potrebbero essere i prametri teorici che ottimizzano il drag
-    
-    #non sono affatto sicura di aver capito come siano collegati rb e drag
+        #controllare se ha senso far fare il report
+        report(e.path, e.history)
 
+    #accedo ai results e calcolo l'infidelity
 
-    return res
+    cov = rb_output.resulst.cov
+    pars = rb_output.results.pars.get(target)
+
+    stdevs = np.sqrt(np.diag(np.reshape(cov[target], (3, 3))))
+    one_minus_p = 1 - pars[2]
+    r_c = one_minus_p * (1 - 1 / 2**1)
+    r_g = r_c / 1.875  # 1.875 is the average number of gates in clifford operation
+    r_c_std = stdevs[2] * (1 - 1 / 2**1)
+    r_g_std = r_c_std / 1.875
+     
+    res = minimize(r_g, init_guess, method = method, options={'xatol': 1e-8, 'disp': True})
+
+    return 0 #capire cosa voglio che questa funzione mi ritorni
 
 target = "D1"
 platform = "qw11q"
 method = 'nelder-mead'
-#definire qual è la tolleranza in maniera intlligente
+#init_guess = 
+#init guess è da determinare sulla base di quali sono i parametri su cui vado a fare ottimizzazione
 
 
 
