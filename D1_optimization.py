7e8a7407d9f1450e9c5aa652e9d3cf905c2369b3
ElStabilini <el.stabilini@gmail.com>
2024-10-17 16:08:45 +0200

    renamed test with 50 iterations and no error saved, added optimization results analysis, and as information the duration of the calibration operation was added at the endo of file

--- D1_optimization.py
+++ D1_optimization.py
@@ -11,7 +11,7 @@ start_time = time.time()
 
 target = "D1"
 platform = "qw11q"
-method = 'BFGS' 
+method = 'nelder-mead' 
 
 executor_path = f'./optimization_data/{target}_{method}'
 opt_history_path = f'./opt_analysis/{target}_{method}'
