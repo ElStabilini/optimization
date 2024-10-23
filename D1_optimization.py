ef66d0ff9a5c73754a113e4f22da8321f70e3ba6
ElStabilini <el.stabilini@gmail.com>
2024-10-23 15:25:08 +0400

    keeping updated the repo

--- D1_optimization.py
+++ D1_optimization.py
@@ -14,8 +14,8 @@ target = "D1"
 platform = "qw11q"
 method = 'nelder-mead' 
 
-executor_path = f'./optimization_data/{target}_{method}'
-opt_history_path = f'./opt_analysis/{target}_{method}'
+executor_path = f'./optimization_data/{target}_{method}_again'
+opt_history_path = f'./opt_analysis/{target}_{method}_again'
 
 with Executor.open(
     "myexec",
