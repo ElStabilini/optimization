a1e704b5c312bff9006d810fc6cca7ae568cfc8f
ElStabilini <el.stabilini@gmail.com>
2024-10-23 16:02:08 +0400

    uploading data from first test of optimization after fine tuning

--- opt_test.sh
+++ opt_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=qw11q 
-#SBATCH --job-name=rb_opt         # Job name
+#SBATCH --job-name=rb_opt_test         # Job name
 
 #run the script
 python D1_optimization.py
