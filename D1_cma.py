84d241ff2e769fa791ee6ff02f3dcd59632c9b4b
ElStabilini <el.stabilini@gmail.com>
2025-02-21 15:28:37 +0100

    start code refcatoring: created separate functions parse, main, update_platform

--- D1_cma.py
+++ D1_cma.py
@@ -2,20 +2,58 @@ import numpy as np
 import os
 import time
 import pickle
-import argparse
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 from qibocal.auto.execute import Executor
 from qibocal.cli.report import report
 from qibolab import pulses
 from cma_opt import rb_optimization
-from pathlib import Path
 
-def main():
-    parser = argparse.ArgumentParser(description="Fine tuning calibration using cma")
-    parser.add_argument("--platform", type=str, required=True, help="Platform identifier")
-    parser.add_argument("--target", type=str, required=True, help="Target qubit to be calibrated") 
-    parser.add_argument("--platform_update", action="store_true", help="Enable platform update")
+NSHOTS = 2000
 
-    args = parser.parse_args()
+
+def parse() -> Namespace:
+    parser = ArgumentParser(description="Fine tuning calibration using cma algorithm")
+    parser.add_argument(
+        "--platform", type=str, required=True, help="Platform identifier"
+    )
+    parser.add_argument(
+        "--target", type=str, required=True, help="Target qubit to be calibrated"
+    )
+    parser.add_argument(
+        "--platform_update", action="store_true", help="Enable platform update"
+    )
+    return parser.parse_args()
+
+
+def update_platform(
+    args: Namespace,
+    params: list,
+):
+    platform = args.platform
+    target = args.target
+    platform_update = args.platform_update
+    executor_path = Path.cwd().parent / "optimization_data" / f"{target}_cma_test"
+
+    e = Executor(
+        "myexec",
+        path=executor_path,
+        platform=platform,
+        targets=[target],
+        update=platform_update,
+        force=True,
+    )
+    e.connect()
+    e.platform.qubits[target].native_gates.RX.amplitude = params[0]  # amplitude
+    e.platform.qubits[target].native_gates.RX.frequency = params[1]  # frequency
+    pulse = e.platform.qubits[target].native_gates.RX.pulse(start=0)
+    rel_sigma = pulse.shape.rel_sigma
+    pulses.Drag(rel_sigma=rel_sigma, beta=params[2])
+    e.disconnect()
+    e.save()
+
+
+def execute(args: Namespace):
     platform = args.platform
     target = args.target
     platform_update = args.platform_update
@@ -34,7 +72,7 @@ def main():
         force=True,
     ) as e:
 
-        e.platform.settings.nshots = 2000
+        e.platform.settings.nshots = NSHOTS
         drag_output = e.drag_tuning(beta_start=-4, beta_end=4, beta_step=0.5)
 
         beta_best = drag_output.results.betas[target]
@@ -42,19 +80,19 @@ def main():
         freq_RX = e.platform.qubits[target].native_gates.RX.frequency
 
         init_guess = np.array([ampl_RX, freq_RX, beta_best])
-
         lower_bounds = np.array([-0.5, freq_RX - 4e6, beta_best - 0.25])
         upper_bounds = np.array([0.5, freq_RX + 4e6, beta_best + 0.25])
         bounds = zip(lower_bounds, upper_bounds)
 
-        opt_results, optimization_history = rb_optimization(e, target, init_guess, bounds)
+        opt_results, optimization_history = rb_optimization(
+            e, target, init_guess, bounds
+        )
 
     report(e.path, e.history)
-
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    # save optimization_history as .npz
+    # Save optimization_history as .npz
     iterations = np.array([step.iteration for step in optimization_history])
     parameters = np.array([step.parameters for step in optimization_history])
     objective_values = np.array([step.objective_value for step in optimization_history])
@@ -72,43 +110,27 @@ def main():
     )
 
     data_stored = {"opt_results": opt_results, "elapsed_time": elapsed_time}
-
     with open(os.path.join(opt_history_path, "optimization_result.pkl"), "wb") as f:
         pickle.dump(data_stored, f)
 
-    #Update platform to best parameters result
+    # Update platform to best parameters result
     fidelities = 1 - objective_values
     sorted_indices_desc = np.argsort(fidelities)[::-1]
 
     sorted_fidelities = fidelities[sorted_indices_desc]
-    sorted_errors = objective_value_error[sorted_indices_desc] 
-    sorted_iterations = iterations[sorted_indices_desc]
+    sorted_errors = objective_value_error[sorted_indices_desc]
     sorted_parameters = parameters[sorted_indices_desc]
 
-    for fidelity, error, iteration, params in zip(
-        sorted_fidelities, sorted_errors, sorted_iterations, sorted_parameters):
-        
-        if fidelity + error < 1:
+    try:
+        idx = np.flatnonzero(sorted_fidelities + sorted_errors > 1)[0]
+        update_platform(platform, sorted_parameters[idx])
+    except IndexError:
+        pass
 
-            e = Executor(
-                "myexec",
-                path=executor_path,
-                platform=platform,
-                targets=[target],
-                update=platform_update,
-                force=True,
-            )
 
-            e.connect()
-            e.platform.qubits[target].native_gates.RX.amplitude = params[0]#amplitude
-            e.platform.qubits[target].native_gates.RX.frequency = params[1]#frequency
-            pulse = e.platform.qubits[target].native_gates.RX.pulse(start=0)    
-            rel_sigma = pulse.shape.rel_sigma
-            pulses.Drag(rel_sigma=rel_sigma, beta=params[2])#beta
-            e.disconnect()
-            e.save()
-            
-            break
+def main():
+    execute(parse())
+
 
 if __name__ == "__main__":
     main()
