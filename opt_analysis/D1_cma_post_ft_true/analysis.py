e622f368901183a1eaced609fab8636749c0033d
ElStabilini <el.stabilini@gmail.com>
2025-07-17 17:54:27 +0200

    refactor: remove analysis

--- opt_analysis/D1_cma_post_ft_true/analysis.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import json
-from argparse import ArgumentParser, Namespace
-
-plt.style.use("/home/elisa/Desktop/Qibo/Qibocal/myplot.mplstyle")
-
-# voglio poter passare gli argomenti con argparse in modo tale da poter in una volta produrre tutte le immagini di cui ho bisogno
